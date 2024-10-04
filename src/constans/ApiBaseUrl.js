e4641c70f9978ea290e15ff9f1f0c47f7b8b239e
wilderif <woojuro3@gmail.com>
2024-10-04 14:48:57 +0900

    feat: Organize constants for tour API and region codes
    - Added constants for the tourism information API
    - Added object of region codes as constants for easier reference

---
+++ src/constans/ApiBaseUrl.js
@@ -0,0 +1,4 @@
+export const BaseUrlTourApi = "apis.data.go.kr/B551011/KorService1";
+
+// 지역 코드 조회
+// https://apis.data.go.kr/B551011/KorService1/areaCode1?&MobileOS=AND&MobileApp=MyFestivalApp&_type=json&serviceKey=w46AZlVErDcysi%2FDs9cpC05M%2F96viVtoZdVutHKKJiK%2BfU9pYBQ%2BdgHFAuQXx2lgQ%2B6au3AVDdUJZJdpyR1v9w%3D%3D
